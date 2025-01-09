7337d15784a88532494a4ff16161c8ec8abf8642
jeenimari <gha0189@gmail.com>
2025-01-09 18:04:09 +0900

    sql 취합

--- RentCarService/src/etc/rentcardb.sql
+++ RentCarService/src/etc/rentcardb.sql
@@ -2,22 +2,78 @@ drop database if exists rentcar;
 create database rentcar;
 use rentcar;
 
-create table grade(
-gno int unsigned auto_increment,
-gname varchar(30) not null,
-gprice int not null,
-mno int,
-constraint primary key (gno),
-constraint foreign key (mno) references model(mno)
+# 차 카테고리 테이블
+create table car (
+	cno int unsigned auto_increment,
+    cname varchar(30) not null unique,
+    constraint primary key(cno)
+);
+
+create table brand (
+	bno int unsigned auto_increment,
+    bname varchar(30) not null unique,
+    cno int unsigned,
+    constraint primary key(bno),
+    constraint foreign key(cno) references car(cno) on update cascade on delete cascade
 );
 
 create table model(
 mno int unsigned auto_increment,
 mname varchar(30) not null unique,
-bno int,
+bno int unsigned,
 constraint primary key(mno),
 constraint foreign key(bno) references brand(bno)
 );
+
+
+create table grade(
+gno int unsigned auto_increment,
+gname varchar(30) not null,
+gprice int not null,
+mno int unsigned,
+constraint primary key (gno),
+constraint foreign key (mno) references model(mno)
+);
+
+create table apply (
+	ano int unsigned auto_increment,
+    aname varchar(30) not null,
+    aphone varchar(13) not null unique,
+    atype int not null,
+    deposit int not null,
+    prepayments int not null,
+    residual_value int not null,
+    duration int not null,
+    constraint primary key(ano)
+);
+
+# 데이터 입력
+# car table
+insert into car(cname) values ('국산차');
+insert into car(cname) values ('수입차');
+
+# brand table
+insert into brand(bname, cno) values ('현대', 1);
+insert into brand(bname, cno) values ('쉐보레', 1);
+insert into brand(bname, cno) values ('기아', 1);
+insert into brand(bname, cno) values ('BMW', 2);
+insert into brand(bname, cno) values ('아우디', 2);
+insert into brand(bname, cno) values ('포드', 2);
+
+#[차모델명 테이블]
+insert into model(mname,bno)values('그랜저','1');
+insert into model(mname,bno)values('소나타','1');
+insert into model(mname,bno)values('타호','2');
+insert into model(mname,bno)values('트래블레이저','2');
+insert into model(mname,bno)values('K5','3');
+insert into model(mname,bno)values('K8','3');
+insert into model(mname,bno)values('3시리즈','4');
+insert into model(mname,bno)values('5시리즈','4');
+insert into model(mname,bno)values('A6','5');
+insert into model(mname,bno)values('A7','5');
+insert into model(mname,bno)values('머스탱','6');
+insert into model(mname,bno)values('익스플로러','6');
+
 #[차등급 테이블 DB]
 insert into grade(gname,gprice,mno)values('프리미엄',38000000,1);
 insert into grade(gname,gprice,mno)values('익스클루시브',45000000,1);
@@ -44,16 +100,15 @@ insert into grade(gname,gprice,mno)values('컨버터블',67000000,11);
 insert into grade(gname,gprice,mno)values('ST-Line',625000000,12);
 insert into grade(gname,gprice,mno)values('PLATINUM',69000000,12);
 
-#[차모델명 테이블]
-insert into model(mname,bno)values('그랜저','1');
-insert into model(mname,bno)values('소나타','1');
-insert into model(mname,bno)values('타호','2');
-insert into model(mname,bno)values('트래블레이저','2');
-insert into model(mname,bno)values('K5','3');
-insert into model(mname,bno)values('K8','3');
-insert into model(mname,bno)values('3시리즈','4');
-insert into model(mname,bno)values('5시리즈','4');
-insert into model(mname,bno)values('A6','5');
-insert into model(mname,bno)values('A7','5');
-insert into model(mname,bno)values('머스탱','6');
-insert into model(mname,bno)values('익스플로러','6');
+# apply table
+insert into apply(aname, aphone, atype, deposit, prepayments, residual_value, duration)
+values ('유재석', '010-1111-1111', 1, 0, 0, 50, 60);
+insert into apply(aname, aphone, atype, deposit, prepayments, residual_value, duration)
+values ('강호동', '010-2222-2222', 1, 30, 30, 40, 48);
+insert into apply(aname, aphone, atype, deposit, prepayments, residual_value, duration)
+values ('신동엽', '010-3333-3333', 2, 50, 50, 30, 36);
+
+# 테이블 확인
+select * from car;
+select * from brand;
+select * from apply;
